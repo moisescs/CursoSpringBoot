116461e846c603d0b17c6ffd639e791bcc91aa92
moisescs <moises.silva_ads@outlook.com>
2019-07-27 08:57:36 -0300

    Aplicando o CSS

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -10,7 +10,8 @@ import javax.persistence.Id;
 @Entity
 public class Evento implements Serializable{
 	
-	private static final long SerialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
 	private long codigo;
