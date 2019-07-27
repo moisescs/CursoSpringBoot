712501c275644d28c80bbf5d3dfae5638381847f
moisescs <moises.silva_ads@outlook.com>
2019-07-27 10:15:35 -0300

    Criando class de convidados

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable{
@@ -21,7 +23,8 @@ public class Evento implements Serializable{
 	private String data;
 	private String horario;
 	
-	
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
@@ -52,6 +55,12 @@ public class Evento implements Serializable{
 	}
 	public void setHorario(String horario) {
 		this.horario = horario;
+	}
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+	public void setConvidados(List<Convidado> convidados) {
+		this.convidados = convidados;
 	}
 	
 }
